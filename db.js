b4c6aee62860a2ad86559f3811f68110c6237dd7
michael-bocim <mtbocim@gmail.com>
2022-11-03 15:56:33 -0600

    routes for get a single company and creating new company

--- db.js
+++ db.js
@@ -2,9 +2,17 @@
 
 const { Client } = require("pg");
 
+//code for Ashley database
+
+// const DB_URI = process.env.NODE_ENV === "test"
+//   ? "postgresql:///biztime_test"
+//   : "postgresql:///biztime";
+
+//code for Michael database
+
 const DB_URI = process.env.NODE_ENV === "test"
-  ? "postgresql:///biztime_test"
-  : "postgresql:///biztime";
+  ? "postgresql://mtbocim:tacocat@localhost/biztime_test"
+  : "postgresql://mtbocim:tacocat@localhost/biztime";
 
 let db = new Client({
   connectionString: DB_URI
--- routes/companies.js
+++ routes/companies.js
@@ -3,6 +3,8 @@ const express = require("express");
 const db = require("../db");
 const router = new express.Router();
 
+const { BadRequestError, NotFoundError } = require("../expressError");
+
 router.get('/', async function (req, res) {
   const results = await db.query(
     `SELECT code, name
@@ -14,5 +16,51 @@ router.get('/', async function (req, res) {
   return res.json({ companies });
 });
 
+router.get('/:code', async function (req, res) {
+  const code = req.params.code;
+  const results = await db.query(
+    `
+    SELECT code, name, description
+      FROM companies
+        WHERE code IN ($1)`,
+    [code]
+  );
 
+  const company = results.rows[0];
+
+  if (!company) throw new NotFoundError(`Not found: ${code}`);
+  return res.json({ company });
+});
+
+router.post('/', async function (req, res) {
+
+  if (req.body === undefined) throw new BadRequestError();
+
+  const requiredKeys = ['code', 'name', 'description'];
+  const passedKeys = Object.keys(req.body);
+
+  if (passedKeys.length !== 3 ||
+    !passedKeys.every(key => requiredKeys.includes(key))
+  ) {
+    throw new BadRequestError();
+  }
+
+  const {code, name, description} = req.body;
+  const result = await db.query(
+    `
+    INSERT INTO companies (code, name, description)
+      VALUES ($1,$2,$3)
+      RETURNING code, name, description
+    `,
+    [code,name,description]
+  );
+
+  const company = result.rows[0]
+
+  return res.status(201).json({company})
+
+})
+
+
+//if (req.body === undefined) throw new BadRequestError();
 module.exports = router;
