536ee8db6723b5a20cc73658ed64242acfd538d8
Ashley Mathew <amathew195@gmail.com>
2022-11-03 19:59:03 -0500

    added company testing for put route

--- routes/companies.test.js
+++ routes/companies.test.js
@@ -8,7 +8,7 @@ const db = require("../db");
 let company;
 let invoice;
 
-beforeEach(async function(){
+beforeEach(async function () {
     await db.query(`DELETE FROM companies`);
     let cResult = await db.query(
         `
@@ -28,10 +28,14 @@ beforeEach(async function(){
         `
     );
     invoice = iResult.rows[0];
-    console.log("invoice>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>", invoice);
+
 });
 
-describe("GET /companies", function(){
+afterAll(async function () {
+    await db.end();
+});
+
+describe("GET /companies", function () {
     test("Get all companies in DB", async function () {
         const resp = await request(app).get("/companies");
         expect(resp.body).toEqual(
@@ -43,20 +47,20 @@ describe("GET /companies", function(){
                     }
                 ]
             }
-        )
-    }); 
+        );
+    });
 });
 
-describe("GET /companies/:code", function(){
-    test("Get a single company from DB", async function() {
+describe("GET /companies/:code", function () {
+    test("Get a single company from DB", async function () {
         const resp = await request(app).get("/companies/testCoCode");
         expect(resp.body).toEqual(
             {
-                company:{
-                    code:'testCoCode',
-                    name:'testCoName',
-                    description:'We test things',
-                    invoices:[
+                company: {
+                    code: 'testCoCode',
+                    name: 'testCoName',
+                    description: 'We test things',
+                    invoices: [
                         invoice.id
                     ]
                 }
@@ -64,7 +68,7 @@ describe("GET /companies/:code", function(){
         );
     });
 
-    test("Attempt nonexistant code from DB", async function(){
+    test("Attempt nonexistant code from DB", async function () {
         const resp = await request(app).get("/companies/notHere");
         expect(resp.statusCode).toEqual(404);
         expect(resp.body).toEqual(
@@ -74,7 +78,108 @@ describe("GET /companies/:code", function(){
                     "status": 404
                 }
             }
-        )
+        );
 
-    })
+    });
+});
+
+describe("POST /companies", function () {
+    test("Add a new company to DB", async function () {
+        const resp = await request(app)
+            .post("/companies")
+            .send({
+                "code": "newCoCode",
+                "name": "newCoName",
+                "description": "newCoDescription"
+            });
+        expect(resp.statusCode).toEqual(201);
+        expect(resp.body).toEqual(
+            {
+                "company":
+                {
+                    "code": "newCoCode",
+                    "name": "newCoName",
+                    "description": "newCoDescription"
+                }
+
+            }
+        );
+    });
+
+    test("Invalid JSON format", async function () {
+        const resp = await request(app)
+            .post("/companies")
+            .send({
+                "code": "newCoCode",
+                "description": "newCoDescription"
+            });
+        expect(resp.statusCode).toEqual(400);
+        expect(resp.body).toEqual(
+            {
+                "error": {
+                    "message": "Invalid JSON format",
+                    "status": 400
+                }
+            }
+        );
+    });
+});
+
+describe("PUT /companies/:code", function () {
+    test("Update existing company in DB", async function () {
+        const resp = await request(app)
+            .put("/companies/testCoCode")
+            .send({
+                "name": "newCoNameUpdate",
+                "description": "newCoDescriptionUpdate"
+            });
+        expect(resp.statusCode).toEqual(200);
+        expect(resp.body).toEqual(
+            {
+                "company":
+                {
+                    "code": "testCoCode",
+                    "name": "newCoNameUpdate",
+                    "description": "newCoDescriptionUpdate"
+                }
+
+            }
+        );
+    });
+
+    test("Invalid JSON format", async function () {
+        const resp = await request(app)
+            .put("/companies/testCoCode")
+            .send({
+                "code": "newCoCode",
+                "description": "newCoDescription"
+            });
+        expect(resp.statusCode).toEqual(400);
+        expect(resp.body).toEqual(
+            {
+                "error": {
+                    "message": "Invalid JSON format",
+                    "status": 400
+                }
+            }
+        );
+    });
+
+    test("Update existing company in DB", async function () {
+        const resp = await request(app)
+            .put("/companies/invalidTestCo")
+            .send({
+                "name": "newCoNameUpdate",
+                "description": "newCoDescriptionUpdate"
+            });
+        expect(resp.statusCode).toEqual(404);
+        expect(resp.body).toEqual(
+            {
+                "error": {
+                    "message": "Not found: invalidTestCo",
+                    "status": 404
+                }
+            }
+        );
+    });
 });
